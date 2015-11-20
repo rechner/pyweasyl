bc40a1753b7fb83b1b595f15a2698fc8c97f2f26
Rechner Fox <jkltechinc@gmail.com>
2015-11-19 23:29:32 -0500

    Add my emberassing setuptools file

--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@ from distutils.core import setup
 
 setup(name='pyweasyl',
       description='Python bindings for the Weasyl API',
-      version='1.0',
+      version='1.1',
       author='Rechner Fox',
       author_email='me@profoundfox.com',
       url='https://github.com/rechner/pyweasyl',
       license='LGPL 3.0',
-      py_modules=['weasyl'])
+      py_modules=['weasyl'],
+      classifiers = [
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3"
+      ])
 
